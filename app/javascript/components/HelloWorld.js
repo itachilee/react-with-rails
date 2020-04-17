1ef9ae79ff79132c403aa90ebbcff10f47194182
itachilee <3050155685@qq.com>
2020-04-17 16:26:39 +0800

    react-rails with ruby2.5 rails5

---
+++ app/javascript/components/HelloWorld.js
@@ -0,0 +1,20 @@
+import React from "react"
+import PropTypes from "prop-types"
+import { Button } from 'antd';
+import { hot } from 'react-hot-loader';
+
+class HelloWorld extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <Button >this ias a Button</Button>
+        Greeting: {this.props.greeting}
+      </React.Fragment>
+    );
+  }
+}
+
+HelloWorld.propTypes = {
+  greeting: PropTypes.string
+};
+export default hot(module)(HelloWorld)
